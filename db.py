5fab00ce12458bad032b4fbbb4ed2e781e1c469b
HarshModi1412 <128783540+HarshModi1412@users.noreply.github.com>
2026-05-04 16:10:30 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,12 +1,11 @@
 import streamlit as st
 from supabase import create_client
 
-# -------- TEMP CONFIG (replace later with st.secrets) --------
-SUPABASE_URL = "https://qaxczmnhygjrletdookk.supabase.co"
-SUPABASE_KEY = "sb_publishable_BpjumdKuYgahI261RSlPcw_5ATBrq6f"
-
 @st.cache_resource
 def get_client():
-    return create_client(SUPABASE_URL, SUPABASE_KEY)
+    return create_client(
+        st.secrets["SUPABASE_URL"],
+        st.secrets["SUPABASE_KEY"]
+    )
 
 supabase = get_client()
